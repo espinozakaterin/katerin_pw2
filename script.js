4ac7fb583a59d72581e77cf3db4aaa5ea66ed34a
espinozakaterin <katherineaguilar575@gmail.com>
2024-06-18 11:45:00 -0600

    repositorio

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() 
+{
+   document.getElementById('bio').innerText = "Soy un apasionado desarrollador web con una amplia experiencia en la construcción y mejora de sitios y aplicaciones web. Mi enfoque se centra en combinar competencias técnicas avanzadas con una visión innovadora para crear experiencias digitales envolventes y efectivas. Con un sólido dominio de los lenguajes de programación, frameworks y tecnologías web, estoy dedicado a proporcionar soluciones de alta calidad que satisfacen las necesidades del usuario y los objetivos empresariales.";
+});
